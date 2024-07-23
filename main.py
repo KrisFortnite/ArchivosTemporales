bca56952d372966b77b023f56c1a40722d241a62
K <129043971+KrisFortnite@users.noreply.github.com>
2024-07-23 04:07:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -75,9 +75,5 @@ def upload_file():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
